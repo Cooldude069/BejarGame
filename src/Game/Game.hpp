bc28db51d32a628405c9a75e6678220ee52f5a61
Samarth <samarthnasnodkar@gmail.com>
2021-04-25 14:15:23 +0530

    Added the base structure and some files.

---
+++ src/Game/Game.hpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+class Game
+{
+private:
+    char *container; // This will hold all the moves
+public:
+    Game()
+    {
+        container = new char[9];
+        for (int i = 0; i < 9; i++)
+        {
+            std::cout << container[i] << std::endl;
+        }
+    }
+    char take_input();     // Will take input from user.
+    char process_input();  // Will process the input.
+    int *possible_moves(); // Give return an array of the number of threats.
+    /*
+    Eg.
+    | | | | Here at position 2, there is a threat, so at index 2 of the array
+    |O|X| | there will be 1. But if u see, some positions are already occupied
+    |X| | | at those positions the value will be -1.
+    */
+    int best_move();    // using the data from possible_moves, we will calculate the best move.
+    bool is_finished(); // Will check if the game is finished or not.
+    Game start();       // Will start the event loop.
+    Game event_loop();  // Will manage the running of the game.
+};
---
+++ src/Game/take_input.hpp
@@ -0,0 +1,7 @@
+char Game::take_input()
+{
+    char inp;
+    std::cout << "Enter the position where you want to place\n";
+    std::cin >> inp;
+    return inp;
+}
